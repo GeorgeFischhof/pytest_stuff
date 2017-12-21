17f4935d8ab04abc7851ce41ad6b43d16f199719
George Fischhof <george@fischhof.hu>
2017-12-21 12:30:17 +0000

    log_messages.py created online with Bitbucket

---
+++ pytest_stuff/create_log_for_module_start_and_finish/log_messages.py
@@ -0,0 +1,11 @@
+
+def start_execution(module_name):
+    return f'\n- - -  Start execution of test script {module_name}  - - -\n'
+
+
+def finish_execution(module_name):
+    return f'\n- - -  Finish execution of test script {module_name}  - - -\n'
+
+
+def check_status_code():
+    return 'Check status code'
