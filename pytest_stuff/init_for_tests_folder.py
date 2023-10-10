bbe4112886e5a97058059f52639f9db69f6ee9f7
GeorgeFischhof <george@fischhof.hu>
2023-10-10 14:59:54 +0200

    Update init_for_tests_folder.py

--- pytest_stuff/init_for_tests_folder.py
+++ pytest_stuff/init_for_tests_folder.py
@@ -3,5 +3,5 @@ import sys
 from pathlib import Path
 
 my_script_folder = Path(__file__).resolve().parent
-my_parent_folder = str(my_script_path.parent)
+my_parent_folder = str(my_script_folder.parent)
 sys.path.insert(1, my_parent_folder)
