0544ff33a36180812055d04d80e9deb4ef9b81ca
GeorgeFischhof <george@fischhof.hu>
2024-02-25 19:50:25 +0100

    some other methods

--- pytest_stuff/warn_the_user_when_there_is_no_mark_on_a_test_case__conftest.py
+++ pytest_stuff/warn_the_user_when_there_is_no_mark_on_a_test_case__conftest.py
@@ -9,5 +9,10 @@ def pytest_collection_modifyitems(items):
     for item in items:
         markers = list(item.iter_markers())
         if not markers:
+            # pytest.fail(f"item {item.nodeid} does not have any marks set")
+            # pytest.skip(f"item {item.nodeid} does not have any marks set")
+            # raise pytest.PytestCollectionWarning(f"item {item.nodeid} does not have any marks set")
+            # raise pytest.PytestUnknownMarkWarning(f"item {item.nodeid} does not have any marks set")
+            # raise pytest.PytestWarning(f"item {item.nodeid} does not have any marks set")
             print(f"item {item.nodeid} does not have any marks set")
             item.add_marker(pytest.mark.skip(reason=f"item {item.nodeid} does not have any marks set"))
